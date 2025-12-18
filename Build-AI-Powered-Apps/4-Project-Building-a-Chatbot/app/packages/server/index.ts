531932971bcb4894b7d628aea40b6ec1f92908ec
Mahmoud-Da <mdabbagh6@gmail.com>
2025-12-18 13:12:08 +0900

    3.1- Extracting Conversation Repository - #30

--- Build-AI-Powered-Apps/4-Project-Building-a-Chatbot/app/packages/server/index.ts
+++ Build-AI-Powered-Apps/4-Project-Building-a-Chatbot/app/packages/server/index.ts
@@ -1,8 +1,9 @@
-import express from "express";
-import type { Request, Response } from "express";
-import OpenAI from "openai";
 import dotenv from "dotenv";
+import type { Request, Response } from "express";
+import express from "express";
+import OpenAI from "openai";
 import { z } from "zod";
+import { conversationRepository } from "./repositories/conversation.repository";
 
 dotenv.config();
 
@@ -24,13 +25,6 @@ app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello World!" });
 });
 
-// method 1
-// only last massage
-// let lastResponseId: string | null = null;
-
-// method 2
-// all conversation
-const conversations = new Map<string, string>();
 const chatSchema = z.object({
   prompt: z
     .string()
@@ -56,15 +50,11 @@ app.post("/api/chat", async (req: Request, res: Response) => {
       // NOTE: this is not support with GPT 5
       // temperature: 0.2,
       max_output_tokens: 100,
-      previous_response_id: conversations.get(conversationId),
+      previous_response_id:
+        conversationRepository.getLastResponseId(conversationId),
     });
 
-    // update in Memory
-    // method 1
-    // PS: gpt5
-    // lastResponseId = response.id;
-
-    conversations.set(conversationId, response.id);
+    conversationRepository.setLastResponseId(conversationId, response.id);
     res.json({
       message: response.output_text,
     });
