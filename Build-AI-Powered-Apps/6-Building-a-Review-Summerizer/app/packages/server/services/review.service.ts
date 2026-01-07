4e7414786ff46c080cfe6699a657f4495cf8f7b6
Mahmoud-Da <mdabbagh6@gmail.com>
2026-01-07 13:47:37 +0900

    3.4- Generating Summaries - #32

--- Build-AI-Powered-Apps/6-Building-a-Review-Summerizer/app/packages/server/services/review.service.ts
+++ Build-AI-Powered-Apps/6-Building-a-Review-Summerizer/app/packages/server/services/review.service.ts
@@ -1,5 +1,10 @@
 import type { Review } from "@prisma/client";
 import { reviewRepository } from "../repositories/review.repository";
+import OpenAI from "openai";
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const reviewService = {
   getReviews(productId: number): Promise<Review[]> {
@@ -10,10 +15,20 @@ export const reviewService = {
     const reviews = await reviewRepository.getReviews(productId, 10);
 
     const joinedReviews = reviews.map((r) => r.content).join("\n\n");
-    // later we will add it to LLM
-    // for testing our api we will just send a string
-    const summary = "This is a placeholder summary";
+    const prompt = `
+      Summarize the following customer reviews into a short paragraph,
+      highlighting key themes, both positive and negative:
 
-    return summary;
+      ${joinedReviews}
+          `;
+    console.log(joinedReviews);
+    const response = await client.responses.create({
+      model: "gpt-5-mini",
+      input: prompt,
+      max_output_tokens: 1000,
+    });
+
+    console.log(response);
+    return response.output_text;
   },
 };
