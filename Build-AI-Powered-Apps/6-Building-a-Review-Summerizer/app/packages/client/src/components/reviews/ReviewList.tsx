2ac83c3ab1ffba10fbbeef866131cccfadc9a32f
Mahmoud-Da <mdabbagh6@gmail.com>
2026-01-12 18:13:39 +0900

    4.6- Displaying the Summary - #32

--- Build-AI-Powered-Apps/6-Building-a-Review-Summerizer/app/packages/client/src/components/reviews/ReviewList.tsx
+++ Build-AI-Powered-Apps/6-Building-a-Review-Summerizer/app/packages/client/src/components/reviews/ReviewList.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect } from "react";
+import { HiSparkles } from "react-icons/hi";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import { Button } from "../ui/button";
 import StarRating from "./StarRating";
 
 type Review = {
@@ -58,24 +59,35 @@ const ReviewList = ({ productId }: ReviewListProps) => {
     return <p className="text-red-500">Could not fetch reviews. Try again.</p>;
   }
 
-  if (!reviewData) return null;
+  if (!reviewData?.reviews.length) return null;
 
   return (
-    <div className="flex flex-col gap-6">
-      {reviewData.summary && (
-        <div className="bg-gray-100 p-4 rounded-md">
-          <h2 className="font-semibold mb-2">Review Summary</h2>
+    <div>
+      <div className="mb-5">
+        {reviewData?.summary ? (
           <p>{reviewData.summary}</p>
-        </div>
-      )}
-
-      {reviewData.reviews.map((review) => (
-        <div key={review.id} className="border p-4 rounded-md">
-          <div className="font-semibold">{review.author}</div>
-          <StarRating value={review.rating} />
-          <p className="py-2">{review.content}</p>
-        </div>
-      ))}
+        ) : (
+          <Button>
+            <HiSparkles />
+            Summarize
+          </Button>
+        )}
+      </div>
+      <div className="flex flex-col gap-6">
+        {reviewData.summary && (
+          <div className="bg-gray-100 p-4 rounded-md">
+            <h2 className="font-semibold mb-2">Review Summary</h2>
+            <p>{reviewData.summary}</p>
+          </div>
+        )}
+        {reviewData.reviews.map((review) => (
+          <div key={review.id} className="border p-4 rounded-md">
+            <div className="font-semibold">{review.author}</div>
+            <StarRating value={review.rating} />
+            <p className="py-2">{review.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
